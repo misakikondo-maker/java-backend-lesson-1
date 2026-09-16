8fe7a94b421af1c794034f3e9f7571aa9a4e94d6
misakikondo-maker <misaki.kondo@leverages.jp>
2026-09-16 04:46:37 +0000

    run.shの修正を完全に反映

--- src/main/java/com/example/demo/OmikujiController.java
+++ src/main/java/com/example/demo/OmikujiController.java
@@ -8,12 +8,10 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api")
 public class OmikujiController {
-/* 
+
    @GetMapping("/omikuji")
     public Map<String, String> getOmikuji() {
         // 問題：String型の変数resultに運勢を代入し文字を表示してみよう！
-         
         return Map.of("result", result);
-    }
-   */     
+    }    
 }
